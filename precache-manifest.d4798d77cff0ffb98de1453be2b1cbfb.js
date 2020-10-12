fa42d56d74347d07a168d08b217904d0ff8c6b38
justpanthering <ankitlakra@outlook.com>
2020-10-12 13:33:11 +0530

    Updates

---
+++ precache-manifest.d4798d77cff0ffb98de1453be2b1cbfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52d273e325adf9b26ad8d880bc141b4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e465fb4d3058a5705d8f",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dbad03f0daaea42b19da",
+    "url": "/portfolio/static/css/main.a9e4918c.chunk.css"
+  },
+  {
+    "revision": "e465fb4d3058a5705d8f",
+    "url": "/portfolio/static/js/2.6a8293e3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.6a8293e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbad03f0daaea42b19da",
+    "url": "/portfolio/static/js/main.8fe118db.chunk.js"
+  },
+  {
+    "revision": "6bafe6fdb4893a0fd8e5",
+    "url": "/portfolio/static/js/runtime-main.459797f6.js"
+  },
+  {
+    "revision": "ddcce61e736ab7ef0b265962013846bc",
+    "url": "/portfolio/static/media/pexels-tirachard-kumtanom-733852.ddcce61e.jpg"
+  }
+]);
